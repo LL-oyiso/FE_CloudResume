7b29029e83625defe87a32d1f9ffd8446b48158b
LL-oyiso <theo.tose.tt@gmail.com>
2024-05-01 12:02:45 +0200

    About me section sectioned

---
+++ cloud-resume/src/components/Aboutme.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Aboutme() {
+  return (
+    <>
+    <div className=' h-[700px] bg-black'>
+        <div className=' justify-around text-white p-4 max-w-[1500px] m-auto  '>
+        <div className=' text-white absolute max-w-[690px] '>
+         <h1 className='font-bold text-[58px] font-[Inter]'>Hello, bro why you not working</h1> 
+       
+       </div>
+    
+     
+        </div>
+    </div>
+    </>
+  )
+}
+export default Aboutme
--- cloud-resume/src/main.jsx
+++ cloud-resume/src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 
  import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
+import Aboutme from './components/Aboutme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Navbar />
     <Hero />
+    <Aboutme />
   </React.StrictMode>,
 )
