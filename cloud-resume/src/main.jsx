f8ccdcd3887a3f742094ceb0d2aee801866cab60
LL-oyiso <theo.tose.tt@gmail.com>
2024-05-01 11:28:22 +0200

    hero component

--- cloud-resume/src/main.jsx
+++ cloud-resume/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Navbar />
+    <Hero />
   </React.StrictMode>,
 )
